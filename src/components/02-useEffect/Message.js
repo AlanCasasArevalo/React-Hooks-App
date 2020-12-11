1e739fa7623913de4ea131b06e38963cdacac1da
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-12-11 06:42:59 +0100

    [FEATURE][USE EFFECT HOOKS] * useEffect was explain in SimpleForm.js, how to mount and unmount listeners to do not have memory leaks

--- src/components/02-useEffect/Message.js
+++ src/components/02-useEffect/Message.js
@@ -1,17 +1,29 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 
 const Message = () => {
 
+    const [coords, setCoords] = useState({x: 0, y: 0})
+    const {x, y} = coords
+
     useEffect(() => {
-            console.log(`El componente fue montado`)
+        const mousemove = (e) => {
+            const coords = {x: e.x, y: e.y}
+            setCoords(coords)
+        }
+
+        window.addEventListener('mousemove', mousemove)
+
+        console.log(`El componente fue montado`)
         return () => {
             console.log(`El componente fue desmontado`)
+            window.removeEventListener('mousemove', mousemove)
         }
     }, [])
 
     return (
         <>
             <h1>Hola </h1>
+            <p> x => {x}, y => {y}</p>
         </>
     );
 };
